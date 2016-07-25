1836e33aa6cc8b7588147d8cc6933310ade95c3e
Antonio López Marín <tonilopezmr@gmail.com>
2016-07-25 15:11:06 +0200

    added validator to injector

--- app/src/main/java/com/tonilopezmr/androidtesting/got/di/CharacterInjector.java
+++ app/src/main/java/com/tonilopezmr/androidtesting/got/di/CharacterInjector.java
@@ -1,6 +1,7 @@
 package com.tonilopezmr.androidtesting.got.di;
 
 import com.tonilopezmr.androidtesting.got.model.CharacterRepository;
+import com.tonilopezmr.androidtesting.got.model.validator.CharacterValidator;
 import com.tonilopezmr.androidtesting.got.presenter.CharacterListPresenter;
 
 public class CharacterInjector {
@@ -8,31 +9,49 @@ public class CharacterInjector {
     private static CharacterInjector injector;
 
     private CharacterListPresenter listPresenter;
-    private CharacterRepository characterCollection;
+    private CharacterRepository characterRepository;
+    private CharacterValidator characterValidator;
+
+    private CharacterValidator characterValidator() {
+        if (characterValidator == null) {
+            return new CharacterValidator();
+        }
+
+        return characterValidator;
+    }
+
+    private CharacterRepository characterRepository() {
+        if (characterRepository == null) {
+            return new CharacterRepository(characterValidator());
+        }
+
+        return characterRepository;
+    }
 
     private CharacterListPresenter characterListPresenter() {
         if (listPresenter == null) {
-            return new CharacterListPresenter(characterCollection());
+            return new CharacterListPresenter(characterRepository());
         }
 
         return listPresenter;
     }
 
-    private CharacterRepository characterCollection() {
-        if (characterCollection == null) {
-            return new CharacterRepository();
-        }
+    private void configService(CharacterValidator characterValidator) {
+        this.characterValidator = characterValidator;
+    }
 
-        return characterCollection;
+    private void configService(CharacterRepository characterRepository) {
+        this.characterRepository = characterRepository;
     }
 
     private void configService(CharacterListPresenter listPresenter) {
         this.listPresenter = listPresenter;
     }
 
-    private void configService(CharacterRepository characterRepository) {
-        this.characterCollection = characterRepository;
-    }
+
+    /////////////////////////////
+    ////      Public API     ////
+    /////////////////////////////
 
     public static void load(CharacterInjector injectorLocator) {
         injector = injectorLocator;
@@ -46,12 +65,16 @@ public class CharacterInjector {
         injector.configService(characterRepository);
     }
 
+    public static void config(CharacterValidator characterValidator) {
+        injector.configService(characterValidator);
+    }
+
     public static CharacterListPresenter injectCharacterListPresenter() {
         return injector.characterListPresenter();
     }
 
-    public static CharacterRepository injectCharacterCollection() {
-        return injector.characterCollection();
+    public static CharacterRepository injectCharacterRepository() {
+        return injector.characterRepository();
     }
 }
 

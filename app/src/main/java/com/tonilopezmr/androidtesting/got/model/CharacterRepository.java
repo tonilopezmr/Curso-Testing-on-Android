ee9f7cde83f052706e4945e241d25cb5922e0568
Antonio López Marín <tonilopezmr@gmail.com>
2016-07-25 15:50:21 +0200

    added okhttp

--- app/src/main/java/com/tonilopezmr/androidtesting/got/model/CharacterRepository.java
+++ app/src/main/java/com/tonilopezmr/androidtesting/got/model/CharacterRepository.java
@@ -5,14 +5,11 @@ import com.google.gson.reflect.TypeToken;
 import com.tonilopezmr.androidtesting.got.model.validator.CharacterValidator;
 import com.tonilopezmr.androidtesting.got.model.validator.InvalidException;
 import com.tonilopezmr.androidtesting.got.model.validator.Validator;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.lang.reflect.Type;
-import java.net.HttpURLConnection;
-import java.net.URL;
 import java.util.*;
 
 
@@ -37,7 +34,7 @@ public class CharacterRepository {
      * @throws Exception
      */
     public List<GoTCharacter> getAll() throws Exception {
-        StringBuffer response = getCharactersFromUrl();
+        StringBuffer response = getCharacters();
 
         Type listType = new TypeToken<ArrayList<GoTCharacter>>() {
         }.getType();
@@ -77,40 +74,22 @@ public class CharacterRepository {
         goTCharacterList.add(goTCharacter);
     }
 
-    protected StringBuffer getCharactersFromUrl() throws Exception {
+    protected StringBuffer getCharacters() throws Exception {
         Thread.sleep(1200); //fake wait
 
         String url =
                 "https://raw.githubusercontent.com/tonilopezmr/Game-of-Thrones/master/app/src/test/resources/data.json";
-        URL obj = new URL(url);
-        HttpURLConnection con = (HttpURLConnection) obj.openConnection();
-        con.setRequestMethod("GET");
-        BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
-        String inputLine;
-        StringBuffer response = new StringBuffer();
-        while ((inputLine = in.readLine()) != null) {
-            response.append(inputLine);
-        }
-        in.close();
-
-        return response;
+        return getCharactersFromUrl(url);
     }
 
-    public StringBuffer getContentFromFile(String filePath) throws Exception {
-        ClassLoader classLoader = getClass().getClassLoader();
-        URL resource = classLoader.getResource(filePath);
-        File file = new File(resource.getPath());
-        return converterFileToString(file);
+    protected StringBuffer getCharactersFromUrl(String endPoint) throws Exception {
+        OkHttpClient client = new OkHttpClient();
+        Request request = new Request.Builder()
+                .url(endPoint)
+                .build();
+
+        Response response = client.newCall(request).execute();
+        return new StringBuffer(response.body().string());
     }
 
-    private StringBuffer converterFileToString(File file) throws Exception {
-        BufferedReader reader = new BufferedReader(new FileReader(file));
-        StringBuffer sb = new StringBuffer();
-        String line;
-        while ((line = reader.readLine()) != null) {
-            sb.append(line).append("\n");
-        }
-        reader.close();
-        return sb;
-    }
 }
